e4c5b36bf43d63c8243fc15fc7471bed342d6688
Hyun Il Kim <goindownblues@gmail.com>
2020-09-19 15:34:19 +0900

    Install dependencies & add headers & add dark theme mode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+
+import { Typography, Paper, Switch } from '@material-ui/core';
+
+import deepOrange from '@material-ui/core/colors/deepOrange';
+import orange from '@material-ui/core/colors/orange';
+
+import Header from '../src/components/Header';
+import HeaderSecond from '../src/components/Header2';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  paper: {
+    width: "100%",
+    height: "100vh"
+  }
+}));
+
 
 function App() {
+  const classes = useStyles();
+  const [dark, setDark] = useState(false);
+
+  const theme = createMuiTheme({
+    palette: {
+      type: dark ? 'dark' : 'light',
+      primary: {
+        main: deepOrange[500]
+      },
+      secondary: {
+        main: orange[500]
+      },
+    },
+  });
+
+  const handleChange = (event) => {
+    setDark((prev) => !prev);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Header dark={dark} handleChange={handleChange}></Header>
+      <HeaderSecond>
+        <Switch
+          checked={dark}
+          onChange={handleChange}
+        />
+      </HeaderSecond>
+
+      <Paper className={classes.paper}>
+        <Typography>Content</Typography>
+      </Paper>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+import IconButton from '@material-ui/core/IconButton';
+import HomeIcon from '@material-ui/icons/Home';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
+import { makeStyles } from '@material-ui/core/styles';
+import { Switch } from '@material-ui/core';
+
+
+const useStyles = makeStyles((theme) => ({
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+    toolbar: {
+        display: 'flex',
+        justifyContent: 'space-between'
+    },
+}));
+
+
+export default function Header({ dark, handleChange }) {
+    const classes = useStyles();
+
+    return (
+        <AppBar position="static">
+            <Toolbar className={classes.toolbar}>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <HomeIcon />
+                </IconButton>
+
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <ShoppingCartIcon />
+                </IconButton>
+
+                <Switch
+                    checked={dark}
+                    onChange={handleChange}
+                />
+            </Toolbar>
+        </AppBar>
+    );
+}
